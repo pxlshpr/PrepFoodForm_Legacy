9980deb17f40c1760dc37af287878008c274b3ab
Ahmed Khalaf <pxlshpr@gmail.com>
2022-09-22 15:42:12 +0500

    bump

--- Sources/PrepFoodForm/Views/MFPSearch.ResultCell.swift
+++ Sources/PrepFoodForm/Views/MFPSearch.ResultCell.swift
@@ -13,9 +13,19 @@ extension MFPSearch.ResultCell {
             self.result = result
             
             let nutrients: [MFPProcessedFood.Nutrient] = [
-                MFPProcessedFood.Nutrient
+                MFPProcessedFood.Nutrient(type: .saturatedFat, amount: 20, unit: .g),
+                MFPProcessedFood.Nutrient(type: .cholesterol, amount: 140, unit: .mg),
+                MFPProcessedFood.Nutrient(type: .addedSugars, amount: 7, unit: .g),
+                MFPProcessedFood.Nutrient(type: .biotin, amount: 64, unit: .mg),
+                MFPProcessedFood.Nutrient(type: .calcium, amount: 54, unit: .mcg),
+                MFPProcessedFood.Nutrient(type: .cobalamin, amount: 2, unit: .mg),
+                MFPProcessedFood.Nutrient(type: .folate, amount: 120, unit: .g),
+//                MFPProcessedFood.Nutrient
             ]
             let sizes: [MFPProcessedFood.Size] = [
+                MFPProcessedFood.Size(quantity: 1, name: "large", amount: 150, amountUnit: .weight, amountVolumeUnit: nil, amountWeightUnit: .g, amountSizeUnit: nil),
+                MFPProcessedFood.Size(quantity: 1, name: "medium", amount: 120, amountUnit: .weight, amountVolumeUnit: nil, amountWeightUnit: .g, amountSizeUnit: nil),
+                MFPProcessedFood.Size(quantity: 1, name: "small", amount: 90, amountUnit: .weight, amountVolumeUnit: nil, amountWeightUnit: .g, amountSizeUnit: nil),
             ]
             let mockFood = MFPProcessedFood(
                 name: "Banana",
@@ -61,6 +71,24 @@ extension MFPSearch.ResultCell.ViewModel {
     var numberOfMicros: Int? {
         processedFood?.nutrients.count
     }
+    
+    var sizes: [MFPProcessedFood.Size]? {
+        processedFood?.sizes
+    }
+    
+    var nutrients: [MFPProcessedFood.Nutrient]? {
+        processedFood?.nutrients
+    }
+    
+    var sizesString: String? {
+        guard let sizes = processedFood?.sizes else { return nil }
+        return sizes.map { $0.name }.joined(separator: ", ")
+    }
+    
+    var nutrientsString: String? {
+        guard let nutrients = processedFood?.nutrients else { return nil }
+        return nutrients.map { $0.type.description.lowercased() }.joined(separator: ", ")
+    }
 }
 
 //MARK: - Cell
@@ -76,44 +104,85 @@ extension MFPSearch {
 
 extension MFPSearch.ResultCell {
     var body: some View {
-        HStack {
+        HStack(alignment: .top) {
             VStack(alignment: .leading, spacing: 5) {
                 Text(viewModel.name)
                     .bold()
-                if let numberOfSizes = viewModel.numberOfSizes, let numberOfMicros = viewModel.numberOfMicros {
-                    HStack {
-                        HStack(spacing: 3) {
-                            Text("\(numberOfSizes)")
-                                .foregroundColor(.secondary)
-                            Text("sizes")
-                                .foregroundColor(Color(.tertiaryLabel))
-                        }
-                        .font(.footnote)
-                        .foregroundColor(.secondary)
-                        .padding(.horizontal, 4)
-                        .padding(.vertical, 2)
-                        .background(
-                            RoundedRectangle(cornerRadius: 7, style: .continuous)
-                                .foregroundColor(Color(.secondarySystemFill))
-                        )
-                        HStack(spacing: 3) {
-                            Text("\(numberOfMicros)")
-                                .foregroundColor(.secondary)
-                            Text("micronutrients")
-                                .foregroundColor(Color(.tertiaryLabel))
-                        }
-                        .font(.footnote)
-                        .padding(.horizontal, 4)
-                        .padding(.vertical, 2)
-                        .background(
-                            RoundedRectangle(cornerRadius: 7, style: .continuous)
-                                .foregroundColor(Color(.secondarySystemFill))
-                        )
-                    }
-                }
+//                summary
+                details
             }
             Spacer()
             NutritionSummary(dataProvider: viewModel)
+        }
+    }
+    
+    @ViewBuilder
+    var details: some View {
+        sizes
+        nutrients
+    }
+    
+    @ViewBuilder
+    var sizes: some View {
+        if let sizesString = viewModel.sizesString {
+            VStack(alignment: .leading) {
+                Text("Sizes")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                Text(sizesString)
+                    .font(.caption2)
+                    .foregroundColor(Color(.tertiaryLabel))
+            }
+        }
+    }
+    
+    
+    @ViewBuilder
+    var nutrients: some View {
+        if let nutrientsString = viewModel.nutrientsString {
+            VStack(alignment: .leading) {
+                Text("Nutrients")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                Text(nutrientsString)
+                    .font(.caption2)
+                    .foregroundColor(Color(.tertiaryLabel))
+            }
+        }
+    }
+
+    @ViewBuilder
+    var summary: some View {
+        if let numberOfSizes = viewModel.numberOfSizes, let numberOfMicros = viewModel.numberOfMicros {
+            HStack {
+                HStack(spacing: 3) {
+                    Text("\(numberOfSizes)")
+                        .foregroundColor(.secondary)
+                    Text("sizes")
+                        .foregroundColor(Color(.tertiaryLabel))
+                }
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .padding(.horizontal, 4)
+                .padding(.vertical, 2)
+                .background(
+                    RoundedRectangle(cornerRadius: 7, style: .continuous)
+                        .foregroundColor(Color(.secondarySystemFill))
+                )
+                HStack(spacing: 3) {
+                    Text("\(numberOfMicros)")
+                        .foregroundColor(.secondary)
+                    Text("micronutrients")
+                        .foregroundColor(Color(.tertiaryLabel))
+                }
+                .font(.footnote)
+                .padding(.horizontal, 4)
+                .padding(.vertical, 2)
+                .background(
+                    RoundedRectangle(cornerRadius: 7, style: .continuous)
+                        .foregroundColor(Color(.secondarySystemFill))
+                )
+            }
         }
     }
 }
