764c95709011468576f0c646d16cc8c1a794f8a2
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-03 23:38:03 +0500

    bump

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
          // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/pxlshpr/SwiftUICamera", from: "0.0.17"),
-        .package(url: "https://github.com/pxlshpr/FoodLabelScanner", from: "0.0.19"),
+        .package(url: "https://github.com/pxlshpr/FoodLabelScanner", from: "0.0.20"),
         .package(url: "https://github.com/pxlshpr/CameraImagePicker", from: "0.0.24"),
         .package(url: "https://github.com/pxlshpr/FoodLabel", from: "0.0.7"),
         .package(url: "https://github.com/pxlshpr/MFPScraper", from: "0.0.54"),
