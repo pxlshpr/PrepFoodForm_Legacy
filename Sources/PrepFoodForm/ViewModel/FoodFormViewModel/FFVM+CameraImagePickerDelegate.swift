73d001b1fd97189fb496ccb78d4b9683cb5f63e8
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-19 10:32:57 +0500

    bump

--- Sources/PrepFoodForm/ViewModel/FoodFormViewModel/FFVM+CameraImagePickerDelegate.swift
+++ Sources/PrepFoodForm/ViewModel/FoodFormViewModel/FFVM+CameraImagePickerDelegate.swift
@@ -26,6 +26,7 @@ extension FoodFormViewModel {
     
     func didScan(_ barcodes: [RecognizedBarcode], on image: UIImage) {
         let imageViewModel = ImageViewModel(image)
+        var didAddABarcode = false
         for barcode in barcodes {
             let fieldViewModel = FieldViewModel(fieldValue:
                     .barcode(FieldValue.BarcodeValue(
@@ -38,10 +39,12 @@ extension FoodFormViewModel {
                         )
                     ))
             )
-            add(barcodeViewModel: fieldViewModel)
+            didAddABarcode = add(barcodeViewModel: fieldViewModel)
         }
-        imageSetStatus = .scanning
-        imageViewModels.append(imageViewModel)
+        if didAddABarcode {
+            imageSetStatus = .scanning
+            imageViewModels.append(imageViewModel)
+        }
     }
     
     public func didPickLibraryImages(numberOfImagesBeingLoaded: Int) {
